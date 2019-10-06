e479cf5b2867089c349bfc2cd989fd8b8aa87a65
Keidi <keidi@knienkotter.local>
2019-10-06 19:21:26 -0300

    exercico 1

--- codelab_dia_02/main.go
+++ codelab_dia_02/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+
+	println("Exercicio 1")
+
+	x := 3
+	y := 5
+
+	println("O maior eh: ")
+	if x > y {
+		fmt.Printf("y = %v", x)
+	} else if x < y {
+		fmt.Printf("y = %v", y)
+	} else {
+		fmt.Print("sao iguais")
+	}
+
 }
