f84627f4b4c27239ab25625a6017e7a28a4ecb29
Keidi <keidi@knienkotter.local>
2019-10-06 18:31:16 -0300

    exercicio 5

--- codelab_dia_01/main.go
+++ codelab_dia_01/main.go
@@ -32,5 +32,27 @@ func main() {
 	// exercício 4
 	fmt.Printf("Soma: %v \nSubtração: %v \nMultiplicação: %v \nDivisão: %v",
 		soma, subtracao, multiplicacao, divisao)
+	fmt.Println("_______________________")
+
+	// exercicio 5
+
+	//Preços dos itens do mercado
+	var preçoDoPão float64 = 4.60
+	//var preçoDaAveia float64 = 5
+	var preçoDoAzeite float64 = 19.95
+	var preçoDoSuco float64 = 7
+	var preçoDaÁgua float64 = 2.15
+	var preçoDoKGMaçã float64 = 8.90
+	//var preçoDoKGBanana float64 = 4.5
+
+	totalCompras := 3 * preçoDoPão
+	totalCompras += preçoDoAzeite
+	totalCompras += 2 * preçoDoSuco
+	totalCompras += 5 * preçoDaÁgua
+	totalCompras += 1.5 * preçoDoKGMaçã
+
+	fmt.Println("Total das compras: ", totalCompras)
+	fmt.Println("_______________________")
+
 
 }
