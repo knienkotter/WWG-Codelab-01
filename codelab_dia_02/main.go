cc8cc6c6cd54e9e1cddaef47d1cb5327f00437a7
Keidi <keidi@knienkotter.local>
2019-10-06 20:07:23 -0300

    exercico 2

--- codelab_dia_02/main.go
+++ codelab_dia_02/main.go
@@ -9,13 +9,22 @@ func main() {
 	x := 3
 	y := 5
 
-	println("O maior eh: ")
+	fmt.Println("O maior eh: ")
 	if x > y {
 		fmt.Printf("y = %v", x)
 	} else if x < y {
 		fmt.Printf("y = %v", y)
 	} else {
 		fmt.Print("sao iguais")
+	}
+
+	fmt.Println("Exercicio 2")
+	ano := 2002
+
+	if ano > (2019 - 16) {
+		fmt.Println("Nao esta apto")
+	} else {
+		fmt.Println("Esta apto")
 	}
 
 }
