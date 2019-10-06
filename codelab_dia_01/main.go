258299a6520e5465e61e143ebac9e56469d6a617
Keidi <keidi@knienkotter.local>
2019-10-06 18:19:11 -0300

    organizando respostas ate exercicio 4

--- codelab_dia_01/main.go
+++ codelab_dia_01/main.go
@@ -1,5 +1,36 @@
 package main
 
 func main() {
+<<<<<<< Updated upstream
 	println("coloque suas resoluções nesse arquivo :D")
+=======
+
+	// exercício 1
+	var numero_um int
+	numero_um = 1
+	fmt.Println(numero_um)
+
+	// exercício 2
+	A := 230
+	B := 27
+	fmt.Println("A: ", A)
+	fmt.Println("B: ", B)
+
+	soma := A + B
+	subtracao := A - B
+	fmt.Println("Soma: ", soma)
+	fmt.Println("Subtração: ", subtracao)
+
+	// exercício 3
+
+	multiplicacao := A * B
+	divisao := A / B
+	fmt.Println("Multiplicação: ", multiplicacao)
+	fmt.Println("Divisão: ", divisao)
+	fmt.Println("_______________________")
+
+	// exercício 4
+	fmt.Printf("Soma: %v \nSubtração: %v \nMultiplicação: %v \nDivisão: %v",
+		soma, subtracao, multiplicacao, divisao)
+>>>>>>> Stashed changes
 }
