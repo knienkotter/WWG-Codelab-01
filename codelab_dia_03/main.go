22c6f26f7cb6bfc1b60ba0852bc9631cf42e66f3
Keidi <keidi@knienkotter.local>
2019-10-06 20:33:40 -0300

    exercico 1

--- codelab_dia_03/main.go
+++ codelab_dia_03/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+
+	println("Exercício 1 ")
+	exercicio1()
+
+}
+
+func exercicio1() {
+	/*
+		Utilizando a estrutura for com regra de incremento +1,
+		faça um programa que printe na tela os números inteiros
+		de 1 a 100 que sejam múltiplos de 3.
+	 */
+	for i := 1; i <= 100; i++ {
+		if i % 3 == 0 {
+			fmt.Println(i)
+		}
+	}
 }
