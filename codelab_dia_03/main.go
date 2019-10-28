3877fe103a53466f3d2e9be68cb5509a7cc3fb6c
Keidi Nienkotter <knienkotter@gmail.com>
2019-10-28 18:51:46 -0300

    adequação novos exercícios

--- codelab_dia_03/main.go
+++ codelab_dia_03/main.go
@@ -9,10 +9,13 @@ func main() {
 	println("Exercício 2 ")
 	exercicio2()
 	println("Exercício 3 ")
-	exercicio3(4, 6, 18)
+	exercicio3(25)
+	exercicio3(28)
 	println("Exercício 4 ")
-	exercicio4(4, 26)
-	exercicio4(3, 7, 92)
+	exercicio4(4, 6, 18)
+	println("Exercício 5 ")
+	exercicio5(4, 26)
+	exercicio5(3, 7, 92)
 
 }
 
@@ -31,11 +34,24 @@ func exercicio1() {
 
 func exercicio2()  {
 	/*
+		Repita o que foi solicitado no exercício 01, mas dessa vez
+		printe na tela apenas os múltiplos de 9 e utilize a keyword
+		continue na sua condicional.
+	 */
+	for i := 1; i <= 100; i++ {
+		if i % 9 != 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
+}
+
+func exercicio3(numero int) {
+	/*
 		Escreva um programa que contenha uma função que
 		teste se um dado número inteiro é múltiplo de 5 ou não.
-	 */
-	numero := 55
-	if numero % 5 == 0 {
+	*/
+	if numero%5 == 0 {
 		fmt.Printf("O numero %v é multiplo de 5", numero)
 	} else {
 		fmt.Printf("O numero %v não é multiplo de 5", numero)
@@ -43,16 +59,16 @@ func exercicio2()  {
 	fmt.Println()
 }
 
-func exercicio3(a int, b int , c int)  {
+func exercicio4(a int, b int , c int) {
 	/*
 		Escreva um programa que contenha uma função
 		que some três operandos e que printe o resultado na tela.
-	 */
+	*/
 	soma := a + b + c
 	fmt.Println("Resultado da soma: ", soma)
 }
 
-func exercicio4(v ...int)  {
+func exercicio5(v ...int)  {
 	/*
 		Escreva um programa que contenha uma função que
 		some n operandos e que printe o resultado na tela.
